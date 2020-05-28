d969d18daa199fd91b3afd438bf07d4342e38e94
umang <umang.rastogi96@gmail.com>
2020-05-28 11:34:16 -0400

    [linked_list.py] Add method to reverse linked list

--- utils/linked_list.py
+++ utils/linked_list.py
@@ -56,19 +56,46 @@ class LinkedList:
         self.size = 0
 
     def get_size(self):
+        """
+        Method to get size of linked list
+        :return: size of linked list
+        """
         return self.size
 
     def add_node(self, data):
+        """
+        Method to add node to linked list
+        :param data: value of new node
+        :return: nothing
+        """
         new_node = ListNode(data, self.head)
         self.head = new_node
         self.size += 1
 
-    def print_node(self):
+    def print_list(self):
+        """
+        Method to print the entire linked list
+        :return: nothing
+        """
         current_node = self.head
         while current_node:
             print(current_node.value, end=' ')
             current_node = current_node.get_next_node()
         print()
+
+    def reverse_list(self):
+        """
+        Method to reverse the linked list
+        :return: nothing
+        """
+        prev_node = None
+        current_node = self.head
+        while current_node is not None:
+            next_node = current_node.get_next_node()
+            current_node.set_next_node(prev_node)
+            prev_node = current_node
+            current_node = next_node
+        self.head = prev_node
 
 
 if __name__ == '__main__':
@@ -84,4 +111,7 @@ if __name__ == '__main__':
         linked_list.add_node(storing_value // divisor)
         storing_value %= divisor
     # Print the linked list
-    linked_list.print_node()
+    linked_list.print_list()
+    # Reverse linked list
+    linked_list.reverse_list()
+    linked_list.print_list()

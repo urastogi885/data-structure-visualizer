03047cd0b527bff3c9b0bc85da1dbc3b0c89e7a7
umang <umang.rastogi96@gmail.com>
2020-05-28 12:00:01 -0400

    [linked_list.py] Remove main
    Add documentation

--- utils/linked_list.py
+++ utils/linked_list.py
@@ -96,22 +96,3 @@ class LinkedList:
             prev_node = current_node
             current_node = next_node
         self.head = prev_node
-
-
-if __name__ == '__main__':
-    # Define value to be stored in the linked list
-    storing_value = 243
-    # Get length of the number
-    digits = len(str(storing_value))
-    # Initialize object of linked list
-    linked_list = LinkedList()
-    # Store each digit of the number
-    for y in range(digits - 1, -1, -1):
-        divisor = 10 ** y
-        linked_list.add_node(storing_value // divisor)
-        storing_value %= divisor
-    # Print the linked list
-    linked_list.print_list()
-    # Reverse linked list
-    linked_list.reverse_list()
-    linked_list.print_list()

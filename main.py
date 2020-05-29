a4aa2af53b0558cf1c5100c516bf2d7bcaa544d0
umang <umang.rastogi96@gmail.com>
2020-05-29 14:35:34 -0400

    [main.py] Use animate-list method

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
         storing_value %= divisor
     # Reverse linked list
     linked_list.reverse_list()
-    linked_list.print_list()
+    linked_list.animate_list()
